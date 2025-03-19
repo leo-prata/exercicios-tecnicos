69c50a751b12716d774a95ee43a1c168e26ec140
leo-prata <leo.prata@estudante.ufjf.br>
2025-03-19 15:30:20 -0300

    envia exercicio 3

---
+++ e3.ts
@@ -0,0 +1,5 @@
+function imprimeArgumentosJuntos(strA: string, strB: string, strC: string, strD: string, strE: string): void {
+  console.log(strA + strB + strC + strD + strE);
+}
+
+console.log(imprimeArgumentosJuntos("Hello", "World", " ! ", " :) ", " '0' "));
